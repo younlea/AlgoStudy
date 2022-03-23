ec54b22200fb1bf2f46214b0fa7afa0ccdec1581
Younlea.kim <younlea.kim@gmail.com>
2022-03-24 05:06:20 +0900

    Add files via upload

---
+++ leetcode/c++/57.Insert Interval.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector <int>> ret;
+        int cnt = 0;
+        int flag = 0;
+        for(int i = 0; i < intervals.size(); i++)
+        {
+            //check first value
+            if(intervals[i][0] >= newInterval[1])    // ][]
+            {
+                
+            }
+            else if(intervals[i][0] <= newInterval[
+                
+            ])
+            {
+                
+            }
+                
+            //add ret
+            ret[i][0] =             
+        }
+    }
+};
