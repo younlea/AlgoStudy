9690e987b8d27693abd6d9ed27a56cd7b1ad3dd3
younlea.kim@samsung.com <younlea.kim@samsung.com>
2022-05-09 05:00:59 +0900

    add leetcode 313 - init

---
+++ [01]leetcode/c++/313. Super Ugly Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+    //n번째까지 모두 구해야 답을 구할수 있음. 
+    //1. primes 값들을 최대값까지 모두 나열한다.       
+    //2. n번째 값을 찾아 낸다. 
+    
+    //     1 <= n <= 105
+    // 1 <= primes.length <= 100
+    // 2 <= primes[i] <= 1000
+    // primes[i] is guaranteed to be a prime number.
+    // All the values of primes are unique and sorted in ascending order.
+    
+    }
+};
