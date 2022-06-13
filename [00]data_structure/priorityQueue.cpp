22f545b4bc1b34c23b8142ebf715dfcf39a27d43
Younlea.kim <younlea.kim@gmail.com>
2022-06-14 05:39:34 +0900

    update

--- [00]data_structure/priorityQueue.cpp
+++ [00]data_structure/priorityQueue.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
-#define heapSize 1500
+#define HEAPSIZE 1500
 int heapcnt = 0;
+int heap[HEAPSIZE] = {0, };
+
+/*
+             [1]           자식 -> 부모 n/2 
+        [2]      [3]       (2n, 2n+1)
+     [4]  [5] [6]   [7]
+
+*/
 
 void push(int data)
 {
